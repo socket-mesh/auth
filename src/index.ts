d50d4d69fb55d8c9a383b049e977a671ce7bdbdc
Greg Kimmy <greg.kimmy@gmail.com>
2023-04-10 22:32:07 -0500

    Added Registered Claim Names to AuthToken

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 export { AuthState, AuthStateChange } from "./auth-state";
 
 export interface AuthToken {
-	[key: string]: any;
+	exp?: number,
+	iss?: string,
+	sub?: string,
+	aud?: string,
+	nbf?: number,
+	iat?: number,
+	jti?: string,
+	[key: string]: any
 }
 
 export type SignedAuthToken = string;
